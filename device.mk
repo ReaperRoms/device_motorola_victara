0f013106ebd82a407d603301635caea136d4227c
LuK1337 <priv.luk@gmail.com>
2016-08-31 11:42:24 +0200

    victara: Build telephony-ext
    Change-Id: Ic5c0404057fec4f68f6a6d52fca3318e94245597

--- device.mk
+++ device.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
     libcurl \
     libxml2
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-victara.conf:system/etc/thermal-engine-victara.conf
