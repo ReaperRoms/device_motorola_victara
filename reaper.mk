b8270c38fac148092da80dd074d28f02c238141e
oraixearl <smith.jesse79@gmail.com>
2016-11-19 05:04:00 -0700

    reaper

--- reaper.mk
+++ reaper.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
-# Inherit some common Broken stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+# Inherit some common Reaper stuff.
+$(call inherit-product, vendor/reaper/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/reaper/config/nfc_enhanced.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -12,12 +12,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 ## Device identifier. This must come after all inclusions
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_DEVICE := victara
-PRODUCT_NAME := broken_victara
+PRODUCT_NAME := reaper_victara
 TARGET_VENDOR := motorola
 PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-# Broken Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Jesse Smith (oraixearl)"
